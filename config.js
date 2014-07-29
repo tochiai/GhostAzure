c1126b67091d7a9be224278dc01da8db858de784
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-07-28 22:06:54 -0700

    Remove unneeded comment

--- config.js
+++ config.js
@@ -9,7 +9,6 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-		// test of deploy
         url: 'http://tylerochiai.azurewebsites.net',
 
         // Example mail config
