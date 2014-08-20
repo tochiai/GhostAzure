0f0cb9ccd5d09e3c5d4f4c77c079f4a3a7909186
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-08-20 13:44:30 -0700

    Add production url

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://www.tylerochiai.com',
         mail: {},
         database: {
             client: 'sqlite3',
