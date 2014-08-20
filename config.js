ece68a0a35772949db40ca7c2d7fac8f314f8984
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-08-20 13:29:43 -0700

    Set url to tylerochiai.com

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'http://tylerochiai.azurewebsites.net',
+        url: 'http://tylerochiai.com',
 
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
